3b3bbddf27208a089e664a9de557c184920bc342
Riyad <riyadk2139@gmail.com>
2026-05-02 22:41:32 +0600

    Initial backend for Railway deployment

--- dist/entities/emergency-contact.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user.entity';
-export declare class EmergencyContact {
-    id: string;
-    name: string;
-    phone: string;
-    relation: string;
-    user: User;
-    userId: string;
-}
--- dist/modules/budget/dto/expense.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateExpenseDto {
-    label: string;
-    category: string;
-    amount: number;
-    type?: string;
-    date?: string;
-}
